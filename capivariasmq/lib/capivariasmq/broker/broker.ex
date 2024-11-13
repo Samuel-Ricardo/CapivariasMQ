4784ce249ffc12abcd7f211d82c428ce910b1c69
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-13 14:23:22 -0300

    [ :sparkles: ] | create: broker

--- capivariasmq/lib/capivariasmq/broker/broker.ex
+++ capivariasmq/lib/capivariasmq/broker/broker.ex
@@ -1,6 +1,6 @@
 defmodule Broker do
-  def dispatch_event(topic) do
-    events = EventManager.dequeue_event(topic)
+  def dispatch_event(events) do
+    # events = EventManager.dequeue_event(topic)
 
     Task.async_stream(events, fn event -> Consumer.process_event(event) end)
     |> Enum.to_list()
