6d9e56f4fdf652c22fa9c8dd66655c7b8ca66c26
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-13 14:24:09 -0300

    [ :sparkles: ] | create: demo (app::main)

--- capivariasmq/lib/capivariasmq.ex
+++ capivariasmq/lib/capivariasmq.ex
@@ -15,4 +15,27 @@ defmodule Capivariasmq do
   def hello do
     :world
   end
+
+  def main do
+    hello()
+    IO.puts("Olá, mundo! Pedro")
+
+    EventManager.start_link(nil)
+
+    EventManager.create_topic("topic_1")
+    #    EventManager.enqueue_event("topic_1", %{id: 1, content: "Evento 1"})
+    #  EventManager.enqueue_event("topic_1", %{id: 2, content: "Evento 2"})
+
+    for i <- 1..100 do
+      EventManager.enqueue_event("topic_1", %{id: i, content: "Evento #{i}"})
+    end
+
+    LoadBalancer.distribute_events("topic_1")
+
+    Process.sleep(1000)
+
+    IO.puts("Olá, mundo!")
+  end
 end
+
+Capivariasmq.main()
