7e7bf726dfabfa383f793799b91a52a23539e584
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-13 12:53:35 -0300

    [ :seedling: ] | init: project :D

---
+++ capivariasmq/lib/capivariasmq.ex
@@ -0,0 +1,18 @@
+defmodule Capivariasmq do
+  @moduledoc """
+  Documentation for `Capivariasmq`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Capivariasmq.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ capivariasmq/test/capivariasmq_test.exs
@@ -0,0 +1,8 @@
+defmodule CapivariasmqTest do
+  use ExUnit.Case
+  doctest Capivariasmq
+
+  test "greets the world" do
+    assert Capivariasmq.hello() == :world
+  end
+end
