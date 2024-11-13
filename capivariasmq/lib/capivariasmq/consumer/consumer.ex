937b4e29be520584840e10f05d1180c7fbfd41a7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-13 18:47:43 -0300

    [ :sparkles: ] | create: consumer - error simulator

--- capivariasmq/lib/capivariasmq/consumer/consumer.ex
+++ capivariasmq/lib/capivariasmq/consumer/consumer.ex
@@ -1,9 +1,22 @@
-defmodule Consumer do
+defmodule Capivariasmq.Consumer do
   @moduledoc """
   Documentation for `Consumer`.
   """
 
+  def process_event_err(event) do
+    case :rand.uniform(10) do
+      n when n <= 8 ->
+        IO.inspect("Successfully processed: #{inspect(event)}")
+        :ok
+
+      _ ->
+        IO.inspect("Error processing: #{inspect(event)}")
+        :error
+    end
+  end
+
   def process_event(event) do
-    IO.inspect("processing event: #{inspect(event)}")
+    IO.inspect("Processing event: #{inspect(event)}")
+    :ok
   end
 end
