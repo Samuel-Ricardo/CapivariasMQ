b664a891c65d39624d2d9b63648662b09c08ab1c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-13 13:07:38 -0300

    [ :sparkles: ] | update: event manager - topics & queue

--- capivariasmq/lib/capivariasmq/event_manager/event_manager.ex
+++ capivariasmq/lib/capivariasmq/event_manager/event_manager.ex
@@ -8,4 +8,16 @@ defmodule EventManager do
   end
 
   def init(state), do: {:ok, state}
+
+  def create_topic(topic) do
+    GenServer.call(__MODULE__, {:create_topic, topic})
+  end
+
+  def enqueue_event(topic, event) do
+    GenServer.call(__MODULE__, {:enqueue_event, topic, event})
+  end
+
+  def dequeue_event(topic) do
+    GenServer.call(__MODULE__, {:dequeue_event, topic})
+  end
 end
